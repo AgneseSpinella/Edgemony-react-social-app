e2084479cf4bdbdd0a74d4a26a78c65476bc817a
Agnese Spinella <agnesespinella@gmail.com>
2022-01-11 18:12:21 +0100

    add loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { useReducer } from "react";
+import { lazy, useReducer } from "react";
 import { Routes, Route } from "react-router-dom";
-
-import NewPost from "./pages/NewPost";
-import Messages from "./pages/Messages";
-import Friends from "./pages/Friends";
-import Login from "./pages/Login";
+import { Suspense } from "react/cjs/react.production.min";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Loading from "./components/Loading";
+//import NewPost from "./pages/NewPost";
+//import Messages from "./pages/Messages";
+//import Friends from "./pages/Friends";
+//import Login from "./pages/Login";
+//import Home from "./pages/Home";
 
-import Home from "./pages/Home";
+
+const NewPost = lazy(() => import("./pages/NewPost"));
+const Messages = lazy(() => import("./pages/Messages"));
+const Friends = lazy(() => import("./pages/Friends"));
+const Login = lazy(() => import("./pages/Login"));
+const Home = lazy(() => import("./pages/Home"));
+
+
+
 
 const INIT_STATE = {
   name: "FeisBell",
@@ -51,11 +61,37 @@ function App() {
       </button>
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new-post" element={<NewPost />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/friends" element={<Friends />} />
-        <Route path="/login" element={<Login />} />
+
+        <Route path="/" element={
+          <Suspense fallback={<Loading/>}>
+              <Home/>
+          </Suspense>
+        }/>
+
+        <Route path="/new-post" element={
+          <Suspense fallback={<Loading/>}>
+            <NewPost />
+          </Suspense>
+        } />
+
+        <Route path="/messages" element={
+          <Suspense fallback={<Loading/>}>
+            <Messages />
+          </Suspense>
+        } />
+
+        <Route path="/friends" element={
+          <Suspense fallback={<Loading/>}>
+            <Friends />
+          </Suspense>
+        } />
+
+        <Route path="/login" element={
+          <Suspense fallback={<Loading/>}>
+            <Login />
+          </Suspense>
+        } />
+        
       </Routes>
 
       <Footer />
---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Loading.module.scss"
+
+const Loading = () => {
+    return (
+        <div className={styles.loading}>
+            Waiting for Loading
+        </div>
+    )
+}
+
+export default Loading
