2afffc96e05ff9e9ee2a1d59ee882eb1ce6c668d
Agnese Spinella <agnesespinella@gmail.com>
2022-01-11 17:08:53 +0100

    prova

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Friends from "./pages/Friends";
 import Login from "./pages/Login";
 
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+//import Header from "./components/Header";
 
 import Home from "./pages/Home";
 
