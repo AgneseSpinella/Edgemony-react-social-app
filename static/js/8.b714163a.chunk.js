73ec9a2944bcb3409a7ea870fad16a2ff65067a7
AgneseSpinella <AgneseSpinella@users.noreply.github.com>
2022-01-11 17:15:02 +0000

    deploy: 6f73b83dc2e1199bcb099ed8ab99cd181131cf06

---
+++ static/js/8.b714163a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsocial-app"]=this["webpackJsonpsocial-app"]||[]).push([[8],{42:function(s,a,c){"use strict";c.r(a);var e=c(1);a.default=function(){return Object(e.jsx)("h1",{children:"Messagesssss"})}}}]);
+//# sourceMappingURL=8.b714163a.chunk.js.map
